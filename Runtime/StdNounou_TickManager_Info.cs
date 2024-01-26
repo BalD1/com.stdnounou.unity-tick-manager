ae1b3aab4b7d325008494b0b17604e1272c27bad
Florian <florianhusson@hotmail.fr>
2024-01-26 18:06:21 +0100

    added define ver

--- Runtime/StdNounou_TickManager_Info.cs
+++ Runtime/StdNounou_TickManager_Info.cs
@@ -1,7 +1,9 @@
+#define STDNOUNOU_TICK_1_0_1
+
 namespace StdNounou.Tick
 {
     public static class StdNounou_TickManager_Info
     {
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.1";
     }
 }
